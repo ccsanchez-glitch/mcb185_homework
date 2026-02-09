6708a235e3c768749a679c7c107c25646c7f760d
ccsanchez-glitch <ccsanchez@ucdavis.edu>
2026-02-09 11:04:56 -0800

    update

---
+++ 10demo.py
@@ -0,0 +1,23 @@
+# 10demo.py by Christina Sanchez 
+
+import math 
+ 
+print ('hello again') #greeting 
+ 
+"""
+ This is a 
+ multi-line 
+ comment 
+""" 
+
+print(1.5e-2)
+print(math.ceil(53))
+print(math.floor(65))
+print(math.log(math.e))
+print(math.log2(8))
+print(math.log10(1000))
+print(math.sqrt(100))
+print(math.pow(2,4))
+print(2**4) 
+print(math.factorial(3))
+
