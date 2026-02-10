b54a2a2ead05c3b4e80d991c3a4e53faa343ad08
ccsanchez-glitch <ccsanchez@ucdavis.edu>
2026-02-09 20:59:52 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -56,3 +56,102 @@ def distance_two_points(x1, y1, x2, y2):
 	return dist 
 	
 print (distance_two_points(2, 3, 5, 7))
+
+
+#strings 
+
+s = 'hello world'
+print(s, type(s))
+
+a=5
+b=3
+if a == b: 
+	print('a equals b')
+	
+if a == b:
+	print('a equals b')
+print(a, b)
+	
+def is_even(x):
+	if x % 2 == 0: return True
+	return False 
+	
+print(is_even(2))
+print(is_even(3))
+
+c = a == b 
+print(c)
+print(type(c))
+	
+	
+if a < b: 
+	print('a < b')
+elif a >b: 
+	print('a > b')
+else:
+	print('a == b')
+	
+if		a < b: 	print('a < b')
+elif 	a > b: 	print('a > b')
+else: 			print('a == b')
+
+if		a < b: 	print('a < b')
+elif 	a >= b: print('a >= b')
+elif 	a == b:	print('this will never print!')
+
+
+a = 0.3 
+b = 0.1 * 3 
+if   a < b: print('a < b')
+elif a > b: print('a > b')
+else: 		print('a ==b')
+
+print(abs(a-b))
+if abs(a-b) < 1e-9: print('close enough')
+
+if math.isclose(a, b): print('close enough')
+
+s1 = 'A'
+s2 = 'B'
+s3 = 'a'
+if s1 < s2: print('A < B')
+if s2 < s3: print('B < a')
+
+#None type, there is no return command in the function 
+
+def silly(m, x, b):
+	y = m * x + b 
+	
+print(silly(2, 3,4))
+
+
+#mismatch type error 
+
+a = 1
+s = 'G'
+# cannot compare an integer to a string 
+
+def is_integer(n): 
+	if n % 1 == 0: return True
+	return False
+	
+print(is_integer(4.5))
+print(is_integer(4))
+
+def is_probability(p): 
+	if p >= 0 and p <= 1: return True 
+	return False 
+	
+print(is_probability(1.5))
+print(is_probability(0.5))
+
+def dna_completment(t):
+	if t == 'A': return 'T'
+	if t == 'C': return 'G' 
+	if t == 'G': return 'C' 
+	if t == 'T': return 'A'
+	return 'None'
+	
+print(dna_completment('T'))
+print(dna_completment('H'))
+
