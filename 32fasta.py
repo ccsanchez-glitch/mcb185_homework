3fcac9edfa10a089ca94047386ce958a27b61a3a
ccsanchez-glitch <ccsanchez@ucdavis.edu>
2026-03-09 20:42:55 -0700

    updated

--- 32fasta.py
+++ 32fasta.py
@@ -1,5 +1,6 @@
 import sys 
 import mcb185
+# defline is an arbitrary variable we made to store the name of each fasta entry 
 
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	print(defline[:30], seq[:40], len(seq))
